1de5a30bf3f8052777006d3a440397c9e8ec6925
Linda Mukami <lindamukami32@gmail.com>
2022-06-02 16:25:44 +0300

    added task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
+if __name__ == "__main__":
 
-print(f"{a} + {b} = ", add_0.add(a,b))
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+print(f"{a} + {b} = ", add(a,b))
